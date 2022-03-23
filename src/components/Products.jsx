a1415487c7e3e233e9676632111546c5a117cb36
Viktor Sivulskiy <info@softmaker.kz>
2022-03-23 11:21:20 +0600

    Grid was added

---
+++ src/components/Products.jsx
@@ -0,0 +1,24 @@
+
+
+
+// GraphQL query for Products
+// {
+//     product(id:"apple-imac-2021") {
+//          name 
+//       prices{
+//         currency {
+//           label
+//           symbol
+//         }
+//         amount
+//       }
+//       description
+//       attributes{
+//         id
+//         items{
+//           displayValue
+//         }
+//       }
+//       gallery
+//     }
+//   }
